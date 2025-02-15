1efa23eae84019f8e881595d83b60cbdbd2e92e4
anuragunnikannan <anuragunnikannan7@gmail.com>
2025-02-15 18:29:50 +0530

    fixed execution script

--- java-execute.sh
+++ java-execute.sh
@@ -8,12 +8,12 @@ password=$2
 username=$3
 internal_ip=$4
 
-command="echo ${password} | sudo -S docker run --rm --network=none -i -v /home/anurag/codes/${dir_name}:/app openjdk java /app/Solution.java < /home/anurag/codes/${dir_name}/input.txt"
+command="echo $password | sudo -S docker run --rm --network=none -i -v /home/anurag/codes/$dir_name:/app openjdk java /app/Solution.java < /home/anurag/codes/$dir_name/input.txt"
 
 # sshpass -p password scp -o StrictHostKeyChecking=no /app/input.sql anurag@ip:/home/anurag/
 
 # command="echo password | sudo -S docker cp /home/anurag/input.sql pg-container:/home/ > /dev/null 2>/dev/null && echo password | sudo -S docker exec -i pg-container psql -U postgres -d postgres < input.sql > /home/anurag/output.txt 2>/dev/null && cat output.txt"
 
-output=`sshpass -p ${password} ssh -q -o StrictHostKeyChecking=no -t ${username}@${internal_ip} ${command}`
+output=`sshpass -p $password ssh -q -o StrictHostKeyChecking=no -t $username@$internal_ip $command`
 
 echo $output
