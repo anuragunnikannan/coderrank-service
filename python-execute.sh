5ec133c9fb2cd6ff8246c5ddea0775b2bdf7285d
anuragunnikannan <anuragunnikannan7@gmail.com>
2025-02-15 20:30:53 +0530

    added script for python

--- python-execute.sh
+++ python-execute.sh
@@ -5,7 +5,7 @@ password=$2
 username=$3
 internal_ip=$4
 
-command="echo $password | sudo -S -p '' bash -c 'docker run --rm --network=none -i -v /home/anurag/codes/$dir_name:/app python3.11-slim python3 /app/solution.py < /home/anurag/codes/$dir_name/input.txt'"
+command="echo $password | sudo -S -p '' bash -c 'docker run --rm --network=none -i -v /home/anurag/codes/$dir_name:/app python:3.11-slim python3 /app/solution.py < /home/anurag/codes/$dir_name/input.txt'"
 
 
 output=`sshpass -p $password ssh -q -o StrictHostKeyChecking=no -t $username@$internal_ip $command`
