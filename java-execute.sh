0b5b77a9a88af83596191256758ae920b80ebcf1
anuragunnikannan <anuragunnikannan7@gmail.com>
2025-02-15 18:52:05 +0530

    added echo statements for debugging

--- java-execute.sh
+++ java-execute.sh
@@ -16,6 +16,6 @@ echo $command
 
 # command="echo password | sudo -S docker cp /home/anurag/input.sql pg-container:/home/ > /dev/null 2>/dev/null && echo password | sudo -S docker exec -i pg-container psql -U postgres -d postgres < input.sql > /home/anurag/output.txt 2>/dev/null && cat output.txt"
 
-# output=`sshpass -p $password ssh -q -o StrictHostKeyChecking=no -t $username@$internal_ip $command`
+output=`sshpass -p $password ssh -q -o StrictHostKeyChecking=no -t $username@$internal_ip $command`
 
-# echo $output
+echo $output
