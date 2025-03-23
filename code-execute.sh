78d33da2c547e1dff88d7a9e67f1c2f6558fcb58
anuragunnikannan <anuragunnikannan7@gmail.com>
2025-03-23 20:36:13 +0530

    fixed shell script not returning output issue

--- code-execute.sh
+++ code-execute.sh
@@ -9,4 +9,4 @@ language=$6
 
 command="echo $password | sudo -S -p '' bash -c 'docker run --rm --network=none --memory=128m -i -v /home/anurag/codes/$dir_name:/codes/ coderrank-execution-service python3 runner.py $mode $language'"
 
-echo $output
+echo $command
